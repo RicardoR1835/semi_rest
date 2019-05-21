7f2d044efae927b3e97f1fe7a67293c9ff45ebf2
Ricardo Rivera <ricardorivera@Ricardos-MacBook-Air.local>
2019-05-21 10:57:24 -0500

    updated

--- apps/show/models.py
+++ apps/show/models.py
@@ -1,4 +1,33 @@
+from __future__ import unicode_literals
 from django.db import models
+from django.core.exceptions import ObjectDoesNotExist
+from datetime import datetime
+date = datetime.now()
+
+print(date)
+print('*' * 80)
+class ShowsManager(models.Manager):
+    def basic_validator(self, postData):
+        errors = {}
+        context = {
+            "show": Shows.objects.all()
+        }
+        grab = Shows.objects.filter(title=postData['title'])
+        if len(postData['title']) < 2:
+            errors['title'] = "Title should be more than 2 characters"
+            print(grab)
+        if len(grab) > 0:
+            errors['title'] = "Title already exists"
+        if len(postData['network']) < 2:
+            errors['network'] = "Network should be more than 2 characters"
+        if len(postData['date']) < 0:
+            errors['date'] = "Please input a date"
+        if postData['date'] > str(date):
+            errors['date'] = "You aint from the future"
+        if len(postData['description']) > 0:
+            if len(postData['description']) < 10:
+                errors['location'] = "Description must be at least 10 characters"
+        return errors
 
 class Shows(models.Model):
     title = models.CharField(max_length=255)
@@ -7,5 +36,7 @@ class Shows(models.Model):
     release_date = models.DateTimeField(null=True, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    objects = ShowsManager()
     def __repr__(self):
         return f"<Shows object: {self.id} ({self.title})>"
--- apps/show/views.py
+++ apps/show/views.py
@@ -1,24 +1,34 @@
 from django.shortcuts import render, HttpResponse, redirect
-from apps.show.models import *
+from .models import *
 from django.contrib.sessions.models import Session
 from datetime import datetime
+from django.contrib import messages
+# date = datetime.now()
+# print(date)
 
 def index(request):
     context = {
         "show": Shows.objects.all()
     }
-    print('*' * 80)
-    print(context)
+    # print('*' * 80)
+    # print(context)
     return render(request, "show/index.html", context)
 
 def newshow(request):
     return render(request, "show/newshow.html")
 
 def addshow(request):
+    errors = Shows.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect('/newshow')
     if request.method == "POST":
         show = Shows.objects.create(title=request.POST['title'], network=request.POST['network'], release_date=request.POST['date'], descriptions=request.POST['description'])
-        print('*' * 80)
-        print(show)
+        # print('*' * 80)
+        messages.success(request, "Show successfully added!")
+        # print(show)
     return redirect(f'show/{show.id}')
 
 def show(request, num):
@@ -26,7 +36,7 @@ def show(request, num):
     context = {
         "show": Shows.objects.get(id=id)
     }
-    print(context)
+    # print(context)
     return render(request, "show/show.html", context)
 
 def edit(request, num):
@@ -35,26 +45,34 @@ def edit(request, num):
     context = {
         "show": Shows.objects.get(id=id)
     }
-    print(context)
+    # print(context)
     return render(request, "show/edit.html", context)
 
 def editshow(request):
     id = request.session['id']
-    if request.method == "POST":
-        show = Shows.objects.get(id=id)
-        print(show.title)
-        show.title = request.POST['title']
-        print(show.title)
-        print(show.network)
-        show.network = request.POST['network']
-        print(show.network)
-        print(show.release_date)
-        show.release_date = request.POST['date']
-        print(show.release_date)
-        show.descriptions = request.POST['description']
-        show.save()
-        print('*' * 80)
-        print(show)
+    errors = Shows.objects.basic_validator(request.POST)
+
+    if len(errors) > 0:
+        for key, value in errors.items():
+            messages.error(request, value)
+        return redirect(f'/edit/{id}')
+    else:
+        if request.method == "POST":
+            show = Shows.objects.get(id=id)
+            # print(show.title)
+            show.title = request.POST['title']
+            # print(show.title)
+            # print(show.network)
+            show.network = request.POST['network']
+            # print(show.network)
+            # print(show.release_date)
+            show.release_date = request.POST['date']
+            # print(show.release_date)
+            show.descriptions = request.POST['description']
+            show.save()
+            # print('*' * 80)
+            # print(show)
+            messages.success(request, "Show successfully editted!")
     return redirect(f"show/{id}")
 
 def destroy(request, num):
